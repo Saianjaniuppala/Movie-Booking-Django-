669dbb2274380de85b39dfc7dc4c9d9640042469
Uppala Sai Anjani <133668580+Saianjaniuppala@users.noreply.github.com>
2025-10-07 19:49:35 +0530

    Create urls.py

---
+++ Movie Booking/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+openapi.Info(
+title="Movie Booking API",
+default_version='v1
